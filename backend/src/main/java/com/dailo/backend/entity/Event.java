0d7317dadf44098a037bd4e1a2c0d7414ab15a97
gupaejwa <sin010423@naver.com>
2026-01-28 02:37:57 +0900

    [FEAT] 이벤트 리스트(필터링) 및 상세 조회 API 구현

--- backend/src/main/java/com/dailo/backend/entity/Event.java
+++ backend/src/main/java/com/dailo/backend/entity/Event.java
@@ -1,17 +1,21 @@
 package com.dailo.backend.entity;
 
+import com.dailo.backend.domain.enums.EventCategory;
+import com.dailo.backend.domain.enums.EventStatus;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "events")
 @Getter
-@Setter
+//@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Event {
 
     @Id
@@ -21,17 +25,47 @@ public class Event {
     @Column(nullable = false, length = 100)
     private String title;
 
-    @Column(length = 200)
-    private String location;
+    @Column(length = 100)
+    private String regionName; // 지역(서울, 경기) -> 필터링용
 
+    @Column(length = 100)
+    private String placeName; // 장소이름
+
+    @Column(length = 200)
+    private String placeAddress; // 주소
+
+    private Double latitude;
+    private Double longitude;
+
+    // +Time 시간 포함 명시
     @Column(name = "start_date", nullable = false)
-    private LocalDateTime startDate;
+    private LocalDateTime startDateTime;
 
     @Column(name = "end_date")
-    private LocalDateTime endDate;
+    private LocalDateTime endDateTime;
 
-    @Column(length = 50)
-    private String category;
+    @Column(length = 255)
+    private String thumbnailUrl;
+
+    @ElementCollection(targetClass = EventCategory.class)
+    @CollectionTable(name = "event_categories", joinColumns = @JoinColumn(name = "event_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    @Builder.Default
+    private List<EventCategory> categories = new ArrayList<>();
+
+    // 공개/비공개 관리
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private EventStatus status = EventStatus.DRAFT;
+
+    // 상세 포스터 이미지(고화질)
+    @ElementCollection
+    @CollectionTable(name = "event_poster_images", joinColumns = @JoinColumn(name = "event_id"))
+    @Column(name = "image_url")
+    @Builder.Default
+    private List<String> posterUrls = new ArrayList<>();
 
     @Column(columnDefinition = "TEXT")
     private String description;
