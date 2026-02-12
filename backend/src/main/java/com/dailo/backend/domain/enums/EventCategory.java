1e4908ff69d60805f5d5a53af9710b58e72bda7e
yunajo-i8 <yunajo5858@gmail.com>
2026-02-13 00:00:12 +0900

    수정

--- backend/src/main/java/com/dailo/backend/domain/enums/EventCategory.java
+++ backend/src/main/java/com/dailo/backend/domain/enums/EventCategory.java
@@ -2,6 +2,6 @@ package com.dailo.backend.domain.enums;
 // 상수 정의
 // 오타 방지 및 규칙 설정
 public enum EventCategory {
-    FESTIVAL, EXHIBITION, TRAFFIC, CONSTRUCTION, ETC
-    // 축제, 전시, 교통(사고/통제), 공사, 기타
+    FESTIVAL, EXHIBITION, PERFORMANCE, EXPERIENCE_BOOTH, FOOD_TRUCK, TRAFFIC, CONSTRUCTION, ETC
+    // 축제, 전시, 공연, 체험부스, 푸드트럭, 교통(사고/통제), 공사, 기타
 }
