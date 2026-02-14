c43d9967c21a65654f6d1a1569c1da02a14e24a6
yuntyu01 <yuntyu01@gmail.com>
2026-02-14 17:23:31 +0900

    [CHORE]: GitHub Actions 배포 파이프라인 구축

--- infra/terraform/live/prod/variables.tf
+++ infra/terraform/live/prod/variables.tf
@@ -3,3 +3,15 @@ variable "db_password" {
   type        = string
   sensitive   = true 
 }
+
+variable "discord_webhook_url" {
+  description = "알람용 디스코드 웹훅 URL"
+  type        = string
+  sensitive   = true 
+}
+
+variable "grafana_admin_password" {
+  description = "그라파나 admin 계정 비밀번호"
+  type        = string
+  sensitive   = true 
+}
