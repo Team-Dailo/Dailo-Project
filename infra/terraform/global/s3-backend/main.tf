5382f989d2062f7d746436e0aad367979f7b49cf
yuntyu01 <yuntyu01@gmail.com>
2026-03-04 23:39:02 +0900

    [FEAT] OIDC 기반 CI/CD 전환 + Terraform/배포 안전조건 및 변수/테스트/문서 정비

--- infra/terraform/global/s3-backend/main.tf
+++ infra/terraform/global/s3-backend/main.tf
@@ -9,11 +9,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "dailo-terraform-state" 
-    key            = "terraform.tfstate"     
-    region         = "ap-northeast-2"
-    encrypt        = true                    
-    dynamodb_table = "terraform-lock-table"  
+    bucket       = "dailo-terraform-state"
+    key          = "terraform.tfstate"
+    region       = "ap-northeast-2"
+    encrypt      = true
+    use_lockfile = true
   }
 
 
@@ -23,9 +23,9 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-##########################
-# S3 + DynamoDB, tfstate #
-##########################
+###############
+# S3, tfstate #
+###############
 
 # 1. 상태 파일(.tfstate)을 저장할 S3 버킷 생성
 resource "aws_s3_bucket" "tf_state" {
@@ -56,18 +56,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encrypti
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-  }
-}
-
-# 4. State Lock을 위한 DynamoDB 테이블 생성
-resource "aws_dynamodb_table" "tf_state_lock" {
-  name         = "terraform-lock-table"
-  billing_mode = "PAY_PER_REQUEST" # 사용한 만큼만 지불 
-  hash_key     = "LockID"
-
-  # Hash Key 이름은 반드시 "LockID"여야 함 (Terraform 규칙)
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
