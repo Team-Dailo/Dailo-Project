5b765f7b7e04725573d20f2ea7dd8550987968fd
yuntyu01 <yuntyu01@gmail.com>
2026-03-09 17:07:50 +0900

    fix: cicd 에러 수정

--- infra/terraform/global/oidc/main.tf
+++ infra/terraform/global/oidc/main.tf
@@ -32,13 +32,15 @@ locals {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # 무의미한 숫자 삽입
 }
 
 data "aws_iam_policy_document" "terraform_assume_role" {
   statement {
     effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+      "sts:TagSession"
+    ]
 
     principals {
       type        = "Federated"
@@ -62,7 +64,10 @@ data "aws_iam_policy_document" "terraform_assume_role" {
 data "aws_iam_policy_document" "deploy_assume_role" {
   statement {
     effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+      "sts:TagSession"
+    ]
 
     principals {
       type        = "Federated"
--- infra/terraform/live/prod/main.tf
+++ infra/terraform/live/prod/main.tf
@@ -74,7 +74,7 @@ resource "aws_ssm_parameter" "jwt_secret" {
 # 1. VPC 모듈
 # ------------------------------------------------------------------------------
 module "vpc" {
-  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/vpc?ref=v0.1.0"
+  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/vpc?ref=v0.1.3"
 
   name     = local.name
   region   = "ap-northeast-2"
@@ -94,7 +94,7 @@ module "vpc" {
 # 2. ecs 모듈
 # ------------------------------------------------------------------------------
 module "ecs" {
-  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/ecs?ref=v0.1.0"
+  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/ecs?ref=v0.1.3"
 
   name   = local.name
   region = "ap-northeast-2" # CloudWatch 로그 등을 위해 사용
@@ -176,7 +176,7 @@ module "ecs" {
 # 3. RDS 모듈
 # ------------------------------------------------------------------------------
 module "rds" {
-  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/rds?ref=v0.1.0"
+  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/rds?ref=v0.1.3"
 
   name   = local.name
   vpc_id = module.vpc.vpc_id
@@ -201,7 +201,7 @@ module "rds" {
 # 4. cdn 모듈
 # ------------------------------------------------------------------------------
 module "cdn" {
-  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/cdn?ref=v0.1.0"
+  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/cdn?ref=v0.1.3"
   
   name        = local.name
   bucket_name = local.static_bucket_name
@@ -220,7 +220,7 @@ module "cdn" {
 # 5. Monitoring (Grafana) 모듈 테스트
 # ------------------------------------------------------------------------------
 module "monitoring" {
-  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/monitoring?ref=v0.1.0" 
+  source = "git::https://github.com/yuntyu01/terraform-aws-modules.git//modules/monitoring?ref=v0.1.3" 
 
   name   = local.name   
   region = "ap-northeast-2"
