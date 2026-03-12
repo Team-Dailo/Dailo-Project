0abcacecf0b49be207bef3eb4f547396c32299b4
yuntyu01 <yuntyu01@gmail.com>
2026-03-12 18:45:08 +0900

    fix: cicd 오류 수정

--- infra/terraform/global/oidc/main.tf
+++ infra/terraform/global/oidc/main.tf
@@ -32,7 +32,6 @@ locals {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # 무의미한 숫자 삽입
 }
 
 data "aws_iam_policy_document" "terraform_assume_role" {
